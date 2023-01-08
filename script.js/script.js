32d5610e739bbbde177b4afe5c169de041a4b0e2
Vanna <vanessa.km7@gmail.com>
2023-01-08 16:49:43 -0300

    Ajustes de layout

--- script.js/script.js
+++ script.js/script.js
@@ -4,6 +4,5 @@ let index = 0;
 function show(increase) {
     index = index + increase;
     index = Math.min(Math.max(index, 0),lista.length -1);
-    lista[index].scrollIntoView({ behavior: "smooth" });
-
+    lista[index].scrollIntoView({behavior: "smooth"});
 }
