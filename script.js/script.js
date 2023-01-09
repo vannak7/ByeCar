67a754b4717952a058bab55749818581ac50c796
Vanna <vanessa.km7@gmail.com>
2023-01-09 15:18:14 -0300

    Adicionando Modal

--- script.js/script.js
+++ script.js/script.js
@@ -6,3 +6,21 @@ function show(increase) {
     index = Math.min(Math.max(index, 0),lista.length -1);
     lista[index].scrollIntoView({behavior: "smooth"});
 }
+
+
+
+const openModalButton = document.querySelector("#open-modal");
+const closeModalButton = document.querySelector("#close-modal");
+const modal = document.querySelector("#modal");
+const fades = document.querySelector("#fades");
+
+const toggleModal = () => {
+    // [modal, fades].forEach((el) => el.classList.toggle("hide"));
+    modal.classList.toggle("hide");
+    fades.classList.toggle("hide");
+};
+
+[openModalButton, closeModalButton, fades].forEach((el) => {
+    el.addEventListener("click", () => toggleModal());
+});
+
